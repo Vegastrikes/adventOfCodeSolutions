88319b7c87ec0ea01d6baea034435903d3f518bc
ozerk <ozerkozay@hotmail.com>
2024-11-05 06:40:46 +0300

    day 1 solutions

---
+++ 2015/d1_p2.go
@@ -0,0 +1,38 @@
+package _2015
+
+import (
+	"adventOfCodeSolutions/utility"
+	"fmt"
+)
+
+func D1_P2() {
+	input, err := utility.GetInput("./2015/inputs/d1_p1.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var floor = 0
+	var position = 0
+
+	for i, c := range input {
+		switch c {
+		case 40:
+			floor++
+		case 41:
+			floor--
+		}
+
+		if floor == -1 {
+			position = i + 1
+			break
+		}
+	}
+
+	if position == 0 {
+		fmt.Println("NO RESULT: Never enter the basement")
+		return
+	}
+
+	fmt.Printf("D1_P2: %d\n", position)
+}
