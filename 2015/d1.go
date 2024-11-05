a866b6c824dd0f2770864fd169fcbd2c4526247b
ozerk <ozerkozay@hotmail.com>
2024-11-05 06:45:59 +0300

    put parts of days in single folder

--- 2015/d1.go
+++ 2015/d1.go
@@ -5,6 +5,30 @@ import (
 	"fmt"
 )
 
+func D1_P1() {
+	input, err := utility.GetInput("./2015/inputs/d1_p1.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var floor = 0
+
+	for _, c := range input {
+		switch c {
+		case 40:
+			floor++
+		case 41:
+			floor--
+		default:
+			fmt.Println("Wrong Input")
+			return
+		}
+	}
+
+	fmt.Printf("D1_P1: %d\n", floor)
+}
+
 func D1_P2() {
 	input, err := utility.GetInput("./2015/inputs/d1_p1.txt")
 	if err != nil {
